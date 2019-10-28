cad73261f9b4aab898dabd20dbda7098833a46b9
Boris Makhlin <borismakhlin5@gmail.com>
2019-10-28 23:13:32 +0200

    Add the second file table

--- FileManager.cs
+++ FileManager.cs
@@ -10,53 +10,77 @@ namespace BobManager
 {
     class FileManager
     {
-        FileTable fileTable = new FileTable();
+        FileTable[] tables = new FileTable[2]
+            {
+                new FileTable(),
+                new FileTable()
+            };
+
+        enum ActiveTable
+        {
+            FirstTableActive,
+            SecondTableActive
+        }
+
+        ActiveTable activeTable = ActiveTable.FirstTableActive;
+
         public FileManager()
         {
             // Set directory
-            fileTable.Dir = new DirectoryInfo(@"C:\Users\boris\Desktop");
+            tables[0].Dir = new DirectoryInfo(@"C:\Users\boris\Desktop");
+            tables[1].Dir = new DirectoryInfo(@"C:\Users\boris\Desktop");
+            tables[1].SelectedItemColor = ConsoleColor.DarkGreen;
+
+            Console.WindowWidth = 125;
 
             Console.BackgroundColor = ConsoleColor.DarkBlue;
             Console.Clear();
         }
-        private static string FormatFileSize(long length)
-        {
-            if (length < Math.Pow(2, 10))
-                return $"{length:0.000} b";
-            if (length >= Math.Pow(2, 10) && length < Math.Pow(2, 20))
-                return $"{length / (double)1000:0.000} kb";
-            if (length >= Math.Pow(2, 20) && length < Math.Pow(2, 30))
-                return $"{length / 1e+6:0.000} mb";
-            if (length >= Math.Pow(2, 30) && length < Math.Pow(2, 40))
-                return $"{length / 1e+9:0.000} gb";
-            throw new FormatException("Bad size");
-        }
+
         private void HandleKeyboard()
         {
             switch (Console.ReadKey().Key)
             {
                 case ConsoleKey.UpArrow:
-                    fileTable.Index--;
+                    tables[(int)activeTable].Index--;
 
                     Console.Clear();
                     Show();
                     break;
                 case ConsoleKey.DownArrow:
-                    fileTable.Index++;
+                    tables[(int)activeTable].Index++;
 
                     Console.Clear();
                     Show();
                     break;
-                case ConsoleKey.Enter:
-                    var items = fileTable.Dir.GetItems().ToList();
+                case ConsoleKey.LeftArrow:
+                    if(activeTable != ActiveTable.FirstTableActive)
+                    {
+                        activeTable = ActiveTable.FirstTableActive;
 
-                    if (items[fileTable.Index] is DirectoryInfo dir)
+                        Console.Clear();
+                        Show();
+                    }
+                    break;
+                case ConsoleKey.RightArrow:
+                    if(activeTable != ActiveTable.SecondTableActive)
+                    {
+                        activeTable = ActiveTable.SecondTableActive;
+
+                        Console.Clear();
+                        Show();
+                    }
+                    break;
+                case ConsoleKey.Enter:
+                    var items = tables[(int)activeTable].Dir.GetItems().ToList();
+
+                    if (items[tables[(int)activeTable].Index] is DirectoryInfo dir)
                     {
                         DirectoryInfo d = new DirectoryInfo(dir.FullName);
-                        fileTable.Dir = d;
-                        fileTable.Index = 0;
+                        tables[(int)activeTable].Dir = d;
+                        tables[(int)activeTable].Index = 0;
                     }
-                    else if (items[fileTable.Index] is FileInfo file)
+                    else if (items[tables[(int)activeTable].Index] is FileInfo file)
                     {
                         Process.Start(file.FullName);
                     }
@@ -77,50 +101,8 @@ namespace BobManager
         }
         private void Show()
         {
-            string title = $"| {"Name",-20} | {"Size (MB)",-16} | {"Date",-10} | {"Time",-5} |";
-            Console.WriteLine(title);
-
-            string line = new string('-', title.Length);
-            Console.WriteLine(line);
-
-            var items = fileTable.Dir.GetItems().ToList();
-
-            int start = fileTable.Index / 20 * 20;
-            for (int i = start; i < (start + 20 > items.Count() ? items.Count() : start + 20); i++)
-            {
-                try
-                {
-                    if (items[i] is DirectoryInfo dir)
-                    {
-                        if (i == fileTable.Index)
-                            Console.BackgroundColor = ConsoleColor.DarkRed;
-
-                        Console.WriteLine($"| {dir.Name.Shorten(20),-20} | " +
-                            $"{"<DIR>",-16} | " +
-                            $"{dir.CreationTime.ToShortDateString()} | " +
-                            $"{dir.CreationTime.ToShortTimeString(),-5} |");
-
-                        if (i == fileTable.Index)
-                            Console.BackgroundColor = ConsoleColor.DarkBlue;
-                    }
-                    else if (items[i] is FileInfo file)
-                    {
-                        if (i == fileTable.Index)
-                            Console.BackgroundColor = ConsoleColor.DarkRed;
-
-                        Console.WriteLine($"| {file.Name.Shorten(20),-20} | " +
-                            $"{FormatFileSize(file.Length),-16:0.000} | " +
-                            $"{file.CreationTime.ToShortDateString()} | " +
-                            $"{file.CreationTime.ToShortTimeString(),-5} |");
-
-                        if (i == fileTable.Index)
-                            Console.BackgroundColor = ConsoleColor.DarkBlue;
-                    }
-                }
-                catch(Exception)
-                {
-                }
-            }
+            tables[0].Draw((0, 0));
+            tables[1].Draw((61, 0));
         }
     }
 }
--- FileTable.cs
+++ FileTable.cs
@@ -12,6 +12,7 @@ namespace BobManager
         private int _index = 0;
 
         public DirectoryInfo Dir { get; set; }
+        public ConsoleColor SelectedItemColor { get; set; } = ConsoleColor.DarkRed;
         public int Index
         {
             get => _index;
@@ -21,6 +22,72 @@ namespace BobManager
 
                 if (value >= 0 && value < itemsCount)
                     _index = value;
+            }
+        }
+        private static string FormatFileSize(long length)
+        {
+            if (length < Math.Pow(2, 10))
+                return $"{length:0.000} b";
+            if (length >= Math.Pow(2, 10) && length < Math.Pow(2, 20))
+                return $"{length / (double)1000:0.000} kb";
+            if (length >= Math.Pow(2, 20) && length < Math.Pow(2, 30))
+                return $"{length / 1e+6:0.000} mb";
+            if (length >= Math.Pow(2, 30) && length < Math.Pow(2, 40))
+                return $"{length / 1e+9:0.000} gb";
+            throw new FormatException("Bad size");
+        }
+        public void Draw((int X, int Y) pos)
+        {
+
+            string title = $"| {"Name",-20} | {"Size (MB)",-12} | {"Date",-10} | {"Time",-5} |";
+
+            Console.SetCursorPosition(pos.X, pos.Y);
+            Console.Write(title);
+
+            string line = new string('-', title.Length);
+
+            Console.SetCursorPosition(pos.X, pos.Y + 1);
+            Console.Write(line);
+
+            var items = Dir.GetItems().ToList();
+
+            int start = Index / 20 * 20;
+            for (int i = start; i < (start + 20 > items.Count() ? items.Count() : start + 20); i++)
+            {
+                try
+                {
+                    if (items[i] is DirectoryInfo dir)
+                    {
+                        if (i == Index)
+                            Console.BackgroundColor = SelectedItemColor;
+
+                        Console.SetCursorPosition(pos.X, pos.Y + (i - start + 2));
+                        Console.Write($"| {dir.Name.Shorten(20),-20} | " +
+                            $"{"<DIR>",-12} | " +
+                            $"{dir.CreationTime.ToShortDateString()} | " +
+                            $"{dir.CreationTime.ToShortTimeString(),-5} |");
+
+                        if (i == Index)
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    }
+                    else if (items[i] is FileInfo file)
+                    {
+                        if (i == Index)
+                            Console.BackgroundColor = SelectedItemColor;
+
+                        Console.SetCursorPosition(pos.X, pos.Y + (i - start + 2));
+                        Console.Write($"| {file.Name.Shorten(20),-20} | " +
+                            $"{FormatFileSize(file.Length),-12:0.000} | " +
+                            $"{file.CreationTime.ToShortDateString()} | " +
+                            $"{file.CreationTime.ToShortTimeString(),-5} |");
+
+                        if (i == Index)
+                            Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    }
+                }
+                catch (Exception)
+                {
+                }
             }
         }
     }
